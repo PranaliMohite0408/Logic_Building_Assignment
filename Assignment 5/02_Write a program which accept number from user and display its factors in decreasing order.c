b6249e77fd3586e74c862934bf4f391770232d53
Pranali Mohite <77424192+PranaliMohite0408@users.noreply.github.com>
2021-09-16 17:27:47 +0530

    Add files via upload

---
+++ Assignment 5/02_Write a program which accept number from user and display its factors in decreasing order.c
@@ -0,0 +1,29 @@
+///Write a program which accept number from user and display its factors in decreasing order.
+
+#include<stdio.h>
+#include<conio.h>
+
+void FactRev(int iNo)
+{
+    int i =0;
+
+    for(i=iNo-1;i>0;i--)
+    {
+        if(iNo%i==0)
+        {
+            printf("\t %d",i);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number => ");
+    scanf("%d",&iValue);
+
+    FactRev(iValue);
+
+    return 0;
+}
